cfcf807934723278dc49b8c3cf727a264c38b755
Janusz Slota <janusz.slota@nixilla.com>
2017-12-21 18:02:51 +0000

    more test for coverage

--- spec/Hubspot/Listener/ErrorListenerSpec.php
+++ spec/Hubspot/Listener/ErrorListenerSpec.php
@@ -3,9 +3,12 @@
 namespace spec\Hubspot\Listener;
 
 use Buzz\Listener\ListenerInterface;
+use Buzz\Message\Request;
+use Buzz\Message\Response;
 use Hubspot\Listener\ErrorListener;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ErrorListenerSpec extends ObjectBehavior
 {
@@ -13,5 +16,25 @@ class ErrorListenerSpec extends ObjectBehavior
     {
         $this->shouldHaveType(ErrorListener::class);
         $this->shouldHaveType(ListenerInterface::class);
+    }
+
+    function it_does_not_implement_preSend(Request $request)
+    {
+        $this->preSend($request);
+    }
+
+    function it_handles_http_errors_and_converts_them_to_exceptions(Request $request, Response $response)
+    {
+        $response->isSuccessful()->willReturn(true);
+        $this->postSend($request, $response);
+
+        $response->isSuccessful()->willReturn(false);
+        $response->getStatusCode()->willReturn(401);
+        $response->getReasonPhrase()->willReturn('Unauthorized');
+
+        $this
+            ->shouldThrow(HttpException::class)
+            ->during('postSend', [$request, $response])
+        ;
     }
 }
